ca7369dcfc2f60667eaa0f6887d2817332eafebc
Lenssen <lenssen.s17@gmail.com>
2025-04-09 15:23:58 +0530

    Completed Dessert

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    int flavor, price;
+    static int numDesserts;
+    public Dessert(int x, int y){
+         flavor = x;
+         price = y;
+        numDesserts++;
+    }
+
+    public void printDessert() {
+        System.out.print(flavor+ " "+ price+ " "+ numDesserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.print("I love dessert!");
+    }
 }
